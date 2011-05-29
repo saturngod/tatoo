4e8410c1aa18c1ce144b9babc1c245ed26f2e0ef
Saturngod <saturngod@gmail.com>
2011-05-29 22:39:05 +0800

    first commit

---
+++ data-provider.js
@@ -0,0 +1,63 @@
+var Db = require('mongodb').Db,
+ObjectID = require('mongodb').BSONNative;
+Server = require('mongodb').Server,
+Connection = require('mongodb').Connection;
+
+var sys= require('sys');
+DataProvider = function(host,port){
+  this.db = new Db('tatoo', new Server(host, port, {}), {native_parser:true});
+  this.db.open(function(){});  
+};
+
+DataProvider.prototype.getCollection = function(callback){
+  this.db.collection('tasks',function(error,task_collection){
+     if(error)  callback(error);
+     else callback(null,task_collection);
+  });
+};
+
+DataProvider.prototype.findAll=function(callback){
+    this.getCollection(function(error,task_collection){
+        if(error)   callback(error);
+        else {
+            task_collection.find({},function(error,cursor){
+                
+                if(error) callback(error);
+                else {
+                    cursor.toArray(function(error,results){
+                    if(error)     callback(error);
+                    else callback(null,results);
+                    });
+                }
+            });
+        }
+    });
+};
+
+DataProvider.prototype.findByUser=function(user,callback){
+    this.getCollection(function(error,task_collection){
+        if(error)   callback(error);
+        else {
+            task_collection.findOne({user:user},function(error,result){
+                if( error ) callback(error)
+                else callback(null, result)
+            });
+        }
+    });
+};
+
+DataProvider.prototype.closeByid=function(user,taskId,callback) {
+    this.getCollection(function(error,task_collection){
+        if(error) callback(error);
+        else {
+            //db.tasks.update({'todo.id':1},{"$set":{"todo.$.done":1}});
+           task_collection.update({user:user,'todo.id':taskId},{"$set":{"todo.$.done":1}}, {safe:true},function(error) {
+               sys.puts("callback user:"+user+"id:"+taskId+"error:"+error);
+               if( error ) callback(error);
+               else callback(null)
+           });
+        }
+    });
+}
+
+exports.DataProvider= DataProvider;
---
+++ index.js
@@ -0,0 +1,34 @@
+var express= require('express');
+var jqtpl = require('jqtpl');
+
+var sys=require('sys');
+
+var app = express.createServer();
+
+var DataProvider= require('./data-provider').DataProvider;
+
+var taskProvider= new DataProvider('localhost', 27017);
+
+app.set("view engine", "html");
+app.register(".html", require("jqtpl").express);
+app.set('views', __dirname + '/views');
+app.set("view options", { layout: true });
+
+app.use("/styles", express.static(__dirname + '/styles'));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+ 
+app.get('/', function(req, res){
+    
+    taskProvider.findByUser("saturngod",function(error, tasks){
+        res.render('index.html',{title:"Tatoo List",list:tasks.todo});
+    });
+    
+});
+
+app.get('/done/:id',function(req,res){
+   taskProvider.closeByid("saturngod",req.params.id,function(error){
+      res.redirect('/');
+   });
+});
+
+app.listen(3000);
